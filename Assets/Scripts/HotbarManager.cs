348fab4e2d218b02ddbf906e3435602b65102eee
Andrei Ristic <aristic2@uwo.ca>
2026-04-15 13:26:56 -0400

    resolved tween warning

--- Assets/Scripts/HotbarManager.cs
+++ Assets/Scripts/HotbarManager.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class HotbarManager : MonoBehaviour {
+public class HotbarManager : MonoBehaviour
+{
     [SerializeField] private ToolBase[] tools;
     [SerializeField] private Image[] slotImages;
     private int active_slot = 0;
+
     public Color normalColor;
     public Color activeColor;
     public Color abilityReadyColor = Color.blue;
 
     public float global_swap_time = 0.5f;
     private float last_swap_time;
-    
+
     private bool paused = false;
 
     public Slider swap_slider;
@@ -27,10 +29,14 @@ public class HotbarManager : MonoBehaviour {
     [SerializeField] private Image abilityIcon;
     [SerializeField] private Slider abilitySlider;
     [SerializeField] private TMPro.TMP_Text abilityText;
-        
-    void Start() {
-        if (tools == null || tools.Length < 3) {
-            Debug.Log ("Tools is null or has insufficient number of tools");
+
+    private Tween swapTween;
+
+    void Start()
+    {
+        if (tools == null || tools.Length < 3)
+        {
+            Debug.Log("Tools is null or has insufficient number of tools");
         }
 
         active_slot = 0;
@@ -39,43 +45,63 @@ public class HotbarManager : MonoBehaviour {
         last_swap_time = -global_swap_time;
     }
 
-    // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (paused) return;
 
-        // if numrow 1 is down
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             SwitchSlot(0);
         }
-        // if numrow 2 is down
-        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             SwitchSlot(1);
         }
-        // if numrow 3 is down
-        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
             SwitchSlot(2);
         }
     }
 
-    public void Pause() {
+    public void Pause()
+    {
         paused = true;
     }
 
-    public void Unpause() {
+    public void Unpause()
+    {
         paused = false;
     }
 
-    private void SwitchSlot(int newSlot) {
+    private void SwitchSlot(int newSlot)
+    {
         if (!(Time.time > last_swap_time + global_swap_time)) return;
-        if (active_slot == newSlot) return; 
-        
-        audioSource.volume = 0.3f;
-        audioSource.PlayOneShot(audioclip);
-        swap_slider.gameObject.SetActive(true);
-        swap_slider.value = 0;
-        swap_slider.DOValue (1, global_swap_time).SetEase (Ease.Linear).OnComplete(() => swap_slider.gameObject.SetActive(false));
+        if (active_slot == newSlot) return;
+
+        if (audioSource != null && audioclip != null)
+        {
+            audioSource.volume = 0.3f;
+            audioSource.PlayOneShot(audioclip);
+        }
+
+        if (swap_slider != null)
+        {
+            swap_slider.DOKill();
+            swap_slider.gameObject.SetActive(true);
+            swap_slider.value = 0;
+
+            swapTween = swap_slider.DOValue(1, global_swap_time)
+                .SetEase(Ease.Linear)
+                .SetLink(swap_slider.gameObject)
+                .OnComplete(() =>
+                {
+                    if (swap_slider == null) return;
+                    swap_slider.gameObject.SetActive(false);
+                });
+        }
+
         last_swap_time = Time.time;
-        
+
         tools[active_slot].Unequip();
         tools[newSlot].Equip();
         slotImages[active_slot].color = normalColor;
@@ -83,18 +109,38 @@ public class HotbarManager : MonoBehaviour {
         active_slot = newSlot;
     }
 
-    public IEnumerator cooldownSlider(int activeSlot, float cooldown) {
+    public IEnumerator cooldownSlider(int activeSlot, float cooldown)
+    {
+        if (activeSlot < 0 || activeSlot >= slotImages.Length) yield break;
+        if (slotImages[activeSlot] == null) yield break;
+
         Slider slider = slotImages[activeSlot].GetComponentInChildren<Slider>(true);
+        if (slider == null) yield break;
+
+        slider.DOKill();
         slider.gameObject.SetActive(true);
         slider.value = 0;
-        slider.DOValue (1, cooldown).SetEase (Ease.Linear).OnComplete(() => slider.gameObject.SetActive(false));
+
+        slider.DOValue(1, cooldown)
+            .SetEase(Ease.Linear)
+            .SetLink(slider.gameObject)
+            .OnComplete(() =>
+            {
+                if (slider == null) return;
+                slider.gameObject.SetActive(false);
+            });
+
         yield return null;
     }
 
     public bool IsOnCooldown(int slot)
     {
+        if (slot < 0 || slot >= slotImages.Length) return false;
+        if (slotImages[slot] == null) return false;
+
         Slider slider = slotImages[slot].GetComponentInChildren<Slider>(true);
         if (slider == null) return false;
+
         return slider.gameObject.activeSelf;
     }
 
@@ -106,7 +152,7 @@ public class HotbarManager : MonoBehaviour {
         if (abilityIcon != null)
         {
             abilityIcon.sprite = icon;
-            abilityIcon.color = abilityReadyColor; // ALWAYS BLUE
+            abilityIcon.color = abilityReadyColor;
         }
 
         if (abilitySlider != null)
@@ -118,7 +164,6 @@ public class HotbarManager : MonoBehaviour {
             else
                 abilitySlider.value = 1f;
 
-            // also keep slider fill blue always
             if (abilitySlider.fillRect != null)
             {
                 Image fillImage = abilitySlider.fillRect.GetComponent<Image>();
@@ -132,5 +177,26 @@ public class HotbarManager : MonoBehaviour {
     {
         if (abilitySlotRoot != null)
             abilitySlotRoot.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        if (swap_slider != null)
+            swap_slider.DOKill();
+
+        if (slotImages != null)
+        {
+            for (int i = 0; i < slotImages.Length; i++)
+            {
+                if (slotImages[i] == null) continue;
+
+                Slider slider = slotImages[i].GetComponentInChildren<Slider>(true);
+                if (slider != null)
+                    slider.DOKill();
+            }
+        }
+
+        if (abilitySlider != null)
+            abilitySlider.DOKill();
     }
 }
